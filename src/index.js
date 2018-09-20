879ec20b1c2e07b1b8e16db161814e7acb9c3f45
Zoe Kavanagh <zoe.kavanagh@Concretes-MacBook-Pro-2.local>
2018-09-20 11:48:37 +0100

    Create drag and drop functionality

---
+++ src/index.js
@@ -0,0 +1,79 @@
+import React, { Component, Fragment } from "react";
+import { render } from "react-dom";
+
+import ReactDropzone from "react-dropzone";
+import request from "superagent";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      files: [],
+    };
+  }
+
+  onDrop = (files) => {
+    // POST to a test endpoint for demo purposes
+    const req = request.post('https://httpbin.org/post');
+
+    files.forEach(file => {
+      req.attach(file.name, file);
+    });
+
+    req.end();
+  }
+
+  onPreviewDrop = (files) => {
+    this.setState({
+      files: this.state.files.concat(files),
+    });
+  }
+
+  render() {
+    const previewStyle = {
+      display: 'inline',
+      width: 100,
+      height: 100,
+    };
+
+    return (
+      <div className="app">
+        <h1>react-dropzone Demo</h1>
+
+        <h2>Basic Example</h2>
+        <ReactDropzone
+          onDrop={this.onDrop}
+        >
+          Drop your best gator GIFs here!!
+        </ReactDropzone>
+
+        <h2>Image Previews</h2>
+        <ReactDropzone
+          accept="image/*"
+          onDrop={this.onPreviewDrop}
+        >
+          Drop an image, get a preview!
+        </ReactDropzone>
+        {this.state.files.length > 0 &&
+          <Fragment>
+            <h3>Previews</h3>
+            {this.state.files.map((file) => (
+              <img
+                alt="Preview"
+                key={file.preview}
+                src={file.preview}
+                style={previewStyle}
+              />
+            ))}
+          </Fragment>
+        }
+
+      </div>
+    );
+  }
+}
+
+const container = document.createElement("div");
+document.body.appendChild(container);
+render(<App />, container);
